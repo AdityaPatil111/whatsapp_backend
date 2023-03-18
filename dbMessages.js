f2aa2f57789680daee2e6110bd3d0b8ff171f8a0
Aditya <adityapatil8975@gmail.com>
2023-03-19 00:30:21 +0530

    pushed
    Co-authored-by: omkarjadhav1123 <omkarjadhav1123@users.noreply.github.com>

--- dbMessages.js
+++ dbMessages.js
@@ -1,13 +1,5 @@
 import mongoose from "mongoose";
 
-// const whatsappSchema=mongoose.Schema({
-//     message:String,
-//     name:String,
-//     timestamp:String,
-//     received:Boolean,
-// });
-
-// export default mongoose.model('messagecontents',whatsappSchema);
 
 const messageSchema = new mongoose.Schema({
     name:String,
